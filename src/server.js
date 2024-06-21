e3c4104ec37cdd4b3ff83b0f1ad898dd4b095428
leetoneee <letoan7442@gmail.com>
2024-06-21 17:18:54 +0700

    add socket

--- src/server.js
+++ src/server.js
@@ -1,34 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
+import http from 'http';
 import configViewEngine from "./config/viewEngine";
 import initApiRoutes from "./routes/api";
 import connectDatabase from "./config/connectDatabase";
+import initSocket from './socket/index';
 
 require("dotenv").config();
 
 let app = express();
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
+    // Pass to next layer of middleware
+    next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,6 +42,11 @@ connectDatabase();
 
 let port = process.env.PORT || 1000;
 
-app.listen(port, () => {
-  console.log("Backend bdraw is running on the port: " + port);
+const server = http.createServer(app);
+
+const io = initSocket(server);
+
+
+server.listen(port, () => {
+    console.log("Backend bdraw is running on the port: " + port);
 });
